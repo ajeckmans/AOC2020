c4f0be705dc4c4f9e075033954d88ef7a91b3ad9
Alexander Jeckmans <ajeckmans@outlook.com>
2020-12-01 08:58:47 +0100

    day 1

--- Program.fs
+++ Program.fs
@@ -1,13 +1,27 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open System.IO
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let rec comb n l =
+    match n, l with
+    | 0, _ -> [ [] ]
+    | _, [] -> []
+    | k, (x :: xs) -> List.map ((@) [ x ]) (comb (k - 1) xs) @ comb k xs
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let filePath = Path.Combine(Directory.GetCurrentDirectory(), argv.[0])
+    let combinationCount = argv.[1] |> int
+
+    let puzzleInput = File.ReadAllLines filePath |> Array.toList |> List.map int
+
+    let answer =
+        puzzleInput
+        |> comb combinationCount
+        |> List.filter (fun l -> List.sum l = 2020)
+        |> List.head
+        |> List.fold (*) 1
+
+    printfn "answer %A" answer
     0 // return an integer exit code
