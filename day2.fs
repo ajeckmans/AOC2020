45200d0ed18d362eba080c2f95e33a913b828405
Alexander Jeckmans <ajeckmans@outlook.com>
2020-12-02 21:31:06 +0100

    perf optimisation

--- day2.fs
+++ day2.fs
@@ -6,23 +6,23 @@ open FSharp.Text.RegexExtensions
 
 type LineRegex = Regex< @"(?<num1>\d+)-(?<num2>\d+) (?<char>.{1}): (?<password>.+)$" >
 
-type passwordRequirement = { num1: int; num2: int; char: char; password: string }
+type LineInput = { num1: int; num2: int; char: char; password: string }
 
-let part1 passwordRequirement =
-    let count =
-        passwordRequirement.password.ToCharArray()
-        |> Array.sumBy (fun c -> if c = passwordRequirement.char then 1 else 0)
+let part1 lineInput =
+    let count = lineInput.password.ToCharArray() |> Array.sumBy (fun c -> if c = lineInput.char then 1 else 0)
 
-    count >= passwordRequirement.num1 && count <= passwordRequirement.num2
+    count >= lineInput.num1 && count <= lineInput.num2
 
-let part2 passwordRequirement =
-    let p1 = passwordRequirement.password.[passwordRequirement.num1 - 1] = passwordRequirement.char
-    let p2 = passwordRequirement.password.[passwordRequirement.num2 - 1] = passwordRequirement.char
+let part2 lineInput =
+    let p1 = lineInput.password.[lineInput.num1 - 1] = lineInput.char
+    let p2 = lineInput.password.[lineInput.num2 - 1] = lineInput.char
     p1 <> p2
 
 let solve filePath passwordValid =
+    let matcher = LineRegex()
+
     let parseLine (line: string) =
-        let parsed = LineRegex().TypedMatch(line)
+        let parsed = matcher.TypedMatch(line)
 
         { num1 = parsed.num1.AsInt
           num2 = parsed.num2.AsInt
